f508bbb7255b1ceb1413ba763571b66800b36352
Jailson-Melo <jailson.melo1@gmail.com>
2021-07-02 18:42:23 -0300

    Styled

--- src/js/script.js
+++ src/js/script.js
@@ -25,27 +25,27 @@ fomrIMC.addEventListener('submit', event => {
 const resultIMC = (imc) => {
     let imcAlert = '';
     if (imc < 18.5) {
-     imcAlert = 'Abaixo do peso';
+     imcAlert = 'Abaixo do peso com um valor de IMC menor que 18.5';
     }
 
     else if (imc >= 18.5 && imc < 25) {
-     imcAlert = 'Peso normal';
+     imcAlert = 'Parabéns, você está no peso normal';
     }
 
     else if (imc >= 25 && imc < 30) {
-     imcAlert = 'Sobrepeso';
+     imcAlert = 'Acho bom você cuidar um pouco da alimentação, está levemente acima do peso!';
     }
 
     else if (imc >= 30 && imc < 35) {
-     imcAlert = 'Obesidade grau 1';
+     imcAlert = 'Obesidade grau 1 é onde começa o problema, melhor você se cuidar!!!';
     }
 
     else if (imc >= 35 && imc < 40) {
-     imcAlert = ' Obesidade grau 2';
+     imcAlert = 'Já chegou na obesidade grau 2? Procure um nutricionista rápido!';
     }
 
     else {
-     imcAlert = 'Obesidade grau 3 ou Mórbida';
+     imcAlert = 'Obesidade grau 3 ou Mórbida, é aqui que a coisa fica mais séria, procure um médico com urgência!';
     }
 
     return imcAlert;
